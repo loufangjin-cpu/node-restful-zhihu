77163a569dbe6b301980924b5d4da9dd2aa117bc
loufangjin <fangjin.lou@deepfinance.com>
2022-09-14 09:43:34 +0800

    feat: mongoose

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -10,7 +10,8 @@ class UserCtl {
     }
     async update(ctx){
         ctx.verifyParams({
-            name:{type: 'string', reauired: true }
+            name:{type: 'string', reauired: false },
+            password:{type: 'string', reauired: false }
         })
         const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
         if(!user) {ctx.throw(404, '用户找不到')}
@@ -18,8 +19,12 @@ class UserCtl {
     }
     async create(ctx){
         ctx.verifyParams({
-            name:{type: 'string', reauired: true }
+            name:{type: 'string', reauired: true },
+            password:{type: 'string', reauired: true }
         })
+        const {name} = ctx.request.body
+        const repeatUser = await User.findOne({name})
+        if(repeatUser) {ctx.throw(409, '用户已经被占用')}
         const user = await new User(ctx.request.body).save()
         ctx.body = user
     }
--- app/models/users.js
+++ app/models/users.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 const {Schema, model} = mongoose
 // 定义user 的文档结构类型
 const userScheme = new Schema({
+    __v: {type: Number, select: false},
     name: {type: String, require: true},
-    age: {type: Number, default: 0}
+    // select: 隐藏密码
+    password: {type: Number, require: true, select: false}
 })
 // 到处用户模型
 module.exports = model('User', userScheme)
